58733d743f05de313215744ab164405783921562
jmejia1920 <jonny86493@gmail.com>
2023-04-12 11:31:51 -0400

    Added new files

---
+++ src/main/java/com/learntocode/ShoppingList.java
@@ -0,0 +1,16 @@
+package com.learntocode;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("1. Apples");
+        System.out.println("2. Bananas");
+        System.out.println("3. Oranges");
+        System.out.println("4. Chicken breast");
+        System.out.println("5. Linguine");
+        System.out.println("6. Heavy cream");
+        System.out.println("7. Broccoli");
+        System.out.println("8. Ground beef");
+        System.out.println("9. Milk");
+        System.out.println("10. Whole wheat bread");
+    }
+}
